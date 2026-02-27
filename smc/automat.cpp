fe6399172d803342b101a458516bf045a68b6124
romanchikova.ad <ndjncnssc@lcp>
2026-02-27 22:25:35 +0500

    попытка 3

--- smc/automat.cpp
+++ smc/automat.cpp
@@ -1,28 +1,35 @@
 #include "automat.h"
 #include <cctype>
+#include <ranges>
+#include <ostream>
+#include <algorithm>
 
-void Automat::check(std::string &str) {
+void Automat::check(const std::string &str) {
     for (const auto &ch : str) {
-        if (isalpha(ch) || ch == '_' || ch == '.') {
-            switch(ch) {
-                case 'c': _fsm.c(); break;
-                case 'r': _fsm.r(); break;
-                case 'e': _fsm.e(); break;
-                case 'a': _fsm.a(); break;
-                case 't': _fsm.t(); break;
-                case 's': _fsm.s(); break;
-                case 'j': _fsm.j(); break;
-                case 'o': _fsm.o(); break;
-                case 'i': _fsm.i(); break;
-                case 'n': _fsm.n(); break;
-                default: _fsm.ch(); break;
-            }
-        } else if (isdigit(ch)) _fsm.num();
-        else if (isspace(ch)) _fsm.sp();
-        else if (ch == '(') _fsm.o_b();
-        else if (ch == ')') _fsm.c_b();
-        else if (ch == ',') _fsm.comma();
-        else _fsm.Unknown();
+        if (!good) { good = true; _fsm.start(); return; }
+        if (isalpha(ch) || ch == '_' || ch == '.') {_fsm.ch(ch); std::cout << "ch" << std::endl;}
+        else if (isdigit(ch)) { _fsm.num(ch); std::cout << "num" << std::endl; }
+        else if (isspace(ch)) {_fsm.sp(); std::cout << "space" << std::endl; }
+        else if (ch == '(') {_fsm.o_b(); std::cout << "(" << std::endl; }
+        else if (ch == ')') {_fsm.c_b(); std::cout << ")" << std::endl; }
+        else if (ch == ',') {_fsm.comma(); std::cout << "comma" << std::endl; }
+        else {_fsm.Unknown(); std::cout << "unknown" << std::endl; }
     }
     _fsm.EOS();
+    if (good) {
+        res_good_string.push_back(str);
+        atribytes.try_emplace(buf_name, buf_all_atr);
+    }
+    _fsm.start();
+}
+
+void Automat::printGood() {
+    for (auto &str : res_good_string) {
+        std::cout << str << std::endl;
+    }
+    for (auto &atr : atribytes) {
+        std::cout << "| " << atr.first << " | ";
+        std::ranges::copy(atr.second, std::ostream_iterator<std::string>(std::cout, " "));
+        std::cout << std::endl;
+    }
 }
